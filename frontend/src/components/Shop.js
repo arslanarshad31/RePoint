22bfa99da1c8d69edaac91d6f14b6dc67e8f3350
arslanarshad31 <arslanarshad.31@gmail.com>
2017-07-08 04:16:24 +0800

    Basic Shop

--- frontend/src/components/Shop.js
+++ frontend/src/components/Shop.js
@@ -1,22 +1,47 @@
-import React from 'react'
-import { Button, Card, Image } from 'semantic-ui-react'
+import React from "react";
+import { Button, Card, Image, Item, Label, Icon } from "semantic-ui-react";
 
-export default class Shop extends React.Component{
-    render(){
-        return (<Card.Group>
-                {["SC", "HSBC", "CITI"].map((v) => {
-                    return (<ShopItem name={v}/>)
-                })}
-            </Card.Group>)
-    }
+export default class Shop extends React.Component {
+  render() {
+    return (
+      <Item.Group divided>
+        {["SC", "HSBC", "CITI"].map(v => {
+          return <ShopItem />;
+        })}
+      </Item.Group>
+    );
+  }
 }
 
-class ShopItem extends React.Component{
-    render(){
-        return (
-            <Card fluid color='orange' header={this.props.name}>
-                <img src="http://placehold.it/50x50" />
-            </Card>
-        )
-    }
+class ShopItem extends React.Component {
+  render() {
+    return (
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <Image
+          src="https://dummyimage.com/100x100"
+          style={{ marginRight: "20px", paddingBottom: "20px" }}
+        />
+        <Item fluid>
+          <Item.Content>
+            <Button
+              primary
+              style={{ position: "absolute", right: "10px", marginTop: "5px" }}
+            >
+              Buy tickets
+              <Icon name="right chevron" />
+            </Button>
+            <Item.Header as="a">12 Years a Slave</Item.Header>
+            <Item.Meta>
+              <span className="cinema">Union Square 14</span>
+            </Item.Meta>
+            <Item.Description>Hi this just some random text</Item.Description>
+            <Item.Extra>
+              <Label>IMAX</Label>
+              <Label icon="globe" content="Additional Languages" />
+            </Item.Extra>
+          </Item.Content>
+        </Item>
+      </div>
+    );
+  }
 }
