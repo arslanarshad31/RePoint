a57c7de05a5cbacd97af63091652bf0284a26ef2
arslanarshad31 <arslanarshad.31@gmail.com>
2017-07-07 22:09:03 +0800

    removed old code

--- frontend/components/button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-class Button extends React.Component{
-    render(){
-        return <button onClick={this.props.onClick}>Increment</button>;
-    }
-}
-
-export default Button;
--- frontend/components/number.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-class Number extends React.Component{
-    render(){
-        return <h2>{this.props.num}</h2>;
-    }
-
-}
-export default Number;
