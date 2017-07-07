8dbd94a5bf172eb986e70dd7919c5ec067ae4d52
arslanarshad31 <arslanarshad.31@gmail.com>
2017-07-07 22:44:25 +0800

    basic redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+let initialState;
+if (typeof(window) !== 'undefined') {
+  initialState = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 }
+
+const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
+
+const App = () => (
+  <Provider store = {store}>
+    <h1>Hello World</h1>
+  </Provider>
+)
 
 export default App;
