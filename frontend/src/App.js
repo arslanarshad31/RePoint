48b64fc8102ebbef2e9e44334e40f938830a52d1
Kshitij Verma <kshitijverma999@hotmail.com>
2017-07-07 23:58:00 +0800

    added semantic css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,8 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import rootReducer from './reducers'
-
+import Dashboard from './components/dashboard'
+import 'semantic-ui-css/semantic.min.css';
 let initialState;
 if (typeof(window) !== 'undefined') {
   initialState = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
@@ -14,7 +15,10 @@ const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
 
 const App = () => (
   <Provider store = {store}>
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+      <Dashboard></Dashboard>
+    </div>
   </Provider>
 )
 
