39ad0d1ad40f90560531337127091bf75089da9a
Kshitij Verma <kshitijverma999@hotmail.com>
2017-07-08 02:41:55 +0800

    added shop component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import rootReducer from './reducers'
 import Dashboard from './components/Dashboard'
 import Bankpage from './components/Bankpage'
+import Shop from './components/Shop'
 import Top from './components/Top'
 import 'semantic-ui-css/semantic.min.css';
 
@@ -17,23 +18,15 @@ if (typeof(window) !== 'undefined') {
 const store = createStore(rootReducer, initialState, applyMiddleware(thunk));
 
 const App = () => (
-<<<<<<< HEAD
-  <Provider store = {store}>
-    <div>
-      <Top />
-      <Bankpage style={{"margin":"20px auto"}}/>
-    </div>
-  </Provider>
-=======
     <Provider store = {store}>
       <Router>
         <div>
           <Route exact path="/" component={Dashboard} />
+          <Route exact path="/shop" component={Shop} />
         </div>
       </Router>
     </Provider>
 
->>>>>>> b09e434203ac5fbaeba87f80eba3e5ede5c438ce
 )
 
 export default App;
--- frontend/src/components/Shop.js
+++ frontend/src/components/Shop.js
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Button, Card, Image } from 'semantic-ui-react'
 
 export default class Shop extends React.Component{
     render(){
-        return
+        return (<Card.Group>
+                {["SC", "HSBC", "CITI"].map((v) => {
+                    return (<ShopItem name={v}/>)
+                })}
+            </Card.Group>)
     }
 }
 
 class ShopItem extends React.Component{
     render(){
-        
+        return (
+            <Card fluid color='orange' header={this.props.name}>
+                <img src="http://placehold.it/50x50" />
+            </Card>
+        )
     }
 }
