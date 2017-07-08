7b072f7194157ac354edae3b937bb07d18e1f731
Kshitij Verma <kshitijverma999@hotmail.com>
2017-07-08 20:57:04 +0800

    made promos page, still need to add images

--- frontend/src/components/Promotions.js
+++ frontend/src/components/Promotions.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Button, Card, Image, Item, Label, Icon } from "semantic-ui-react";
 
 export default class Promotions extends React.Component {
@@ -6,7 +6,7 @@ export default class Promotions extends React.Component {
     return (
       <Item.Group divided>
         {["SC", "HSBC", "CITI"].map(v => {
-          return <PromotionItem/>;
+          return <PromotionItem />;
         })}
       </Item.Group>
     );
@@ -16,7 +16,61 @@ export default class Promotions extends React.Component {
 class PromotionItem extends React.Component {
   render() {
     return (
-      <div style={{ display: "flex", flexDirection: "row", marginBottom: '10px' }}>
+      <div
+        style={{
+          borderRadius: "10px",
+          backgroundColor: "#ffffff",
+          boxShadow: "0 2px 18px 0 #e5e5e5",
+          height: "539px",
+          marginBottom: "15px"
+        }}
+      >
+        <div
+          style={{
+            height: "330px",
+            borderTopLeftRadius: "10px",
+            borderTopRightRadius: "10px",
+            background: "url('https://dummyimage.com/900x900') no-repeat",
+            marginBottom: "15px"
+          }}
+        />
+        <div style={{ padding: "5px 15px" }}>
+          <div>
+            <span
+              style={{
+                fontSize: "4.37vh",
+                fontWeight: "bold",
+                letterSpacing: "-1px",
+                textAlign: "left",
+                color: "#4b4d5a"
+              }}
+            >
+              Heading goes here
+            </span>
+            <span style={{ float: "right" }}>B</span>
+          </div>
+          <div
+            style={{
+              fontFamily: "SFUIText",
+              fontSize: "3.5vh",
+              lineHeight: "1.1",
+              letterSpacing: "-0.9px",
+              textAlign: "left",
+              color: "#a9a9af",
+              marginTop: "15px"
+            }}
+          >
+            The city of southern California, san diego is locally known as
+            ‘America’s Finest City’. It’s located on San Diego Bay, an inlet of
+            the Pacific Ocean near the...
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+/*
+<div style={{ display: "flex", flexDirection: "row", marginBottom: '10px' }}>
         <Image
           src="https://dummyimage.com/100x100"
           style={{ marginRight: "20px"}}
@@ -42,6 +96,4 @@ class PromotionItem extends React.Component {
           </Item.Content>
         </Item>
       </div>
-    );
-  }
-}
+      */
