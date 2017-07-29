35fcac110cd6aafa88f8fe4702cc2d8419e100ca
Arslan Arshad <arslanarshad.31@gmail.com>
2017-07-29 12:19:46 +0800

    Changed API URL

--- frontend/src/actions/index.js
+++ frontend/src/actions/index.js
@@ -1,9 +1,9 @@
 import request from 'superagent'
 
 function getData_helper() {
-    return request.get("http://54.255.134.151:5000/api/all").then(data => {
+    return request.get("http://repoints.tech:5000/api/all/").then(data => {
       let main = JSON.parse(data.text)
-        return request.get("http://54.255.134.151:5000/api/demoAccountData/").then(acc => {
+        return request.get("http://repoints.tech:5000/api/demoAccountData/").then(acc => {
           main.Account = JSON.parse(acc.text)
           return main;
         })
